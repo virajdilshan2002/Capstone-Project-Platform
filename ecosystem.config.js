3a88e7c0058c6b976cd2ded2bc2a0dc85fd9fbf9
virajdilshan2002 <virajdilshan2019@gmail.com>
2026-03-26 22:47:46 +0530

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   apps : [{
     name   : "config-server",
-    script : "java -jar ./config-server/target/Config-Server-1.0.0.jar"
+    script : "java -jar ./config-server/target/Config-Server-1.0.0.jar",
+    log_file : "./logs/config-server.log",
   },
     {
       name   : "service-registry",
-      script : "./service-registry/target/Service-Registry-1.0.0.jar"
+      script : "./service-registry/target/Service-Registry-1.0.0.jar",
+      log_file : "./logs/service-registry.log"
     },
     {
       name   : "api-gateway",
-      script : "./api-gateway/target/Api-Gateway-1.0.0.jar"
+      script : "./api-gateway/target/Api-Gateway-1.0.0.jar",
+      log_file : "./logs/api-gateway.log"
     }]
 }
