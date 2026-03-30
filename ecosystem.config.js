b9f17e9db5e4ba6f138cb8f30b541b68968cfb4c
virajdilshan2002 <virajdilshan2019@gmail.com>
2026-03-30 21:32:39 +0530

    update ecosystem.config.js to run services with 'java -jar' command

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
   },
     {
       name   : "service-registry",
-      script : "./service-registry/target/Service-Registry-1.0.0.jar",
+      script : "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
       log_file : "./logs/service-registry.log"
     },
     {
       name   : "api-gateway",
-      script : "./api-gateway/target/Api-Gateway-1.0.0.jar",
+      script : "java -jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
       log_file : "./logs/api-gateway.log"
     }]
 }
